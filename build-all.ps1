1f186cbdaf5ae863a791bdc9d2cd647f08c92923
Isaac Springer <idspringer@onyxhat.com>
2021-03-09 21:10:40 -0600

    refactor

--- build-all.ps1
+++ build-all.ps1
@@ -1,6 +1,7 @@
 $MyPath = Split-Path $MyInvocation.MyCommand.Definition
 $BinDir = if (!$env:BINARY_OUTDIR) { "bin" } else { $env:BINARY_OUTDIR }
 $MyProject = if (!$env:CI_REPOSITORY_NAME_SLUG) { $(Get-Item $MyPath).BaseName } else { $env:CI_REPOSITORY_NAME_SLUG }
+$BuildArch = if (!$env:BUILD_ARCH) { ((& go tool dist list) -match '^(darwin|linux|windows)/(arm|arm64|386|amd64)$') -join ',' } else { $env:BUILD_ARCH }
 
 if (!(Test-Path $MyPath/$BinDir)) { New-Item -Path $MyPath/$BinDir -ItemType Directory | Out-Null }
 
@@ -8,21 +9,19 @@ Push-Location $MyPath
 
 & go get .
 
-ForEach ($g in $(& go tool dist list)) {
-    if ($g -match '^(darwin|linux|windows)/(arm|arm64|386|amd64)$') {
-        $env:GOOS = $Matches[1]
-        $env:GOARCH = $Matches[2]
+foreach ($g in $BuildArch.Split(',')) {
+    $env:GOOS = $g.split('/')[0]
+    $env:GOARCH = $g.split('/')[1]
 
-        if ($env:GOOS -eq "windows") { $Ext = ".exe" } else { $Ext = $null }
+    if ($env:GOOS -eq "windows") { $Ext = ".exe" } else { $Ext = $null }
 
-        Try {
-            Write-Host "Building: ${env:GOOS} (${env:GOARCH})"
-            & go build -ldflags="-s -w" -o "${MyPath}/${BinDir}/${MyProject}-${env:GOOS}-${env:GOARCH}${Ext}"
-        }
+    try {
+        Write-Host "Building: ${env:GOOS} (${env:GOARCH})"
+        & go build -ldflags="-s -w" -o "${MyPath}/${BinDir}/${MyProject}-${env:GOOS}-${env:GOARCH}${Ext}"
+    }
 
-        Catch {
-            Write-Warning ("Build Failed:`r`n" + $_.Exception.Message)
-        }
+    catch {
+        Write-Warning ("Build Failed:`r`n" + $_.Exception.Message)
     }
 }
 
